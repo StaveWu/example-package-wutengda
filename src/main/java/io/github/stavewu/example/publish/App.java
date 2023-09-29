207cf314ddd546e58f5f8df49c02fe6c4ee9d63a
stavewu <wtengda0816@outlook.com>
2023-09-30 00:08:48 +0800

    rename group id, add public method

---
+++ src/main/java/io/github/stavewu/example/publish/App.java
@@ -0,0 +1,21 @@
+package io.github.stavewu.example.publish;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    /**
+     * greeting the world.
+     */
+    public void greeting() {
+        System.out.println("Hello world!");
+    }
+
+    public static void main( String[] args )
+    {
+        App app = new App();
+        app.greeting();
+    }
+}
